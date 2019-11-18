54be718aa6ddd59ad36fe047759b15712bb22c4b
IkkyOdufade <ikmah_odufade@yahoo.com>
2019-11-18 18:13:08 +0100

    CHANGED WEBPACK

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,38 @@
-var webpack = require('webpack');
+var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry:'../src/index.js',
+  entry: './index.js',
   output: {
-    path:(__dirname +'/src'),
-    filename: 'index.js'
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude:/(node_modules|bower_components)/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['env']
           }
         }
-      }, 
+      }, {
+        test: /\.*css$/,
+        use : ExtractTextPlugin.extract({
+            fallback : 'style-loader',
+            use : [
+                'css-loader',
+                'sass-loader'
+            ]
+        })
+       },
     ]
+  },
+  externals: {
+    'react': 'commonjs react' 
   }
 };
